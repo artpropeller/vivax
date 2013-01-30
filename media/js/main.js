8f73759ef05a2525932913a853a4d68ec0d6a5bb
bugerman <aelfer23@yandex.ru>
2013-01-30 14:11:44 +0300

    Му change

--- media/js/main.js
+++ media/js/main.js
@@ -1,8 +1,20 @@
-$(function(){
-    if ($('body').width()<1600) $('#top_bg').width(723-(1600-$('body').width()));
-    if ($('body').width()<1025) {$('#social').css('left','1000px');} else {$('#social').css('left','auto');}
-    $(window).resize(function(){
-        if ($('body').width()<1600) $('#top_bg').width(723-(1600-$('body').width()));
-        if ($('body').width()<1025) {$('#social').css('left','1000px');} else {$('#social').css('left','auto');}
+$(function () {
+    if ($('body').width() < 1600) $('#top_bg').width(723 - (1600 - $('body').width()));
+    if ($('body').width() < 1025) {
+        $('#social').css('left', '1000px');
+    } else {
+        $('#social').css('left', 'auto');
+    }
+    $(window).resize(function () {
+        if ($('body').width() < 1600) $('#top_bg').width(723 - (1600 - $('body').width()));
+        if ($('body').width() < 1025) {
+            $('#social').css('left', '1000px');
+        } else {
+            $('#social').css('left', 'auto');
+        }
     });
+
+
+
+
 });
