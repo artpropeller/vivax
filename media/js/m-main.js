5c683fc170c500767d0f76930b21f2ff8fcdbd0e
Ivan Aleksanov <zloi.gremlin@gmail.com>
2013-02-04 17:09:59 +0400

    radio fix

---
+++ media/js/m-main.js
@@ -0,0 +1,56 @@
+$(document).bind('pageinit', function () {
+    if ($('body').width() < 1700) $('#top_bg').width(700 - (1700 - $('body').width()));
+    if ($('body').width() < 1025) {
+        $('#social').css('left', '1000px');
+        $('#top_bg').css('right', '-44px');
+    } else {
+        $('#social').css('left', 'auto');
+        $('#top_bg').css('right', '0');
+    }
+    $(window).resize(function () {
+        if ($('body').width() < 1700) $('#top_bg').width(700 - (1700 - $('body').width()));
+        if ($('body').width() < 1025) {
+            $('#social').css('left', '1000px');
+            $('#top_bg').css('right', '-44px');
+        } else {
+            $('#social').css('left', 'auto');
+            $('#top_bg').css('right', '0');
+        }
+    });
+
+    $('.tabs .item').vclick(function () {
+        if (!$(this).hasClass('active')) {
+            $('.tabs .item').removeClass('active');
+            $(this).addClass('active');
+            $('.vkladishi').hide(0);
+            $($(this).attr('rel')).show(0);
+        }
+        return false;
+    });
+
+
+    $('.desk-list li h2').vclick(function () {
+        $(this).parent().find('.quote').slideToggle(200);
+        return false;
+
+    });
+
+    $('.checkbox input').change(function () {
+        $(this).parent().toggleClass('active');
+    });
+
+
+    $('.radio').click(function () {
+        if (!$(this).is('active')) {
+            $('.radio[rel=' + $(this).attr('rel') + ']').removeClass('active');
+            $(this).addClass('active');
+        }
+
+    });
+
+
+});
+
+
+
+
