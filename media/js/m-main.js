35d362a5b8aa7c398909406e42a7467b79f32d11
Ivan Aleksanov <zloi.gremlin@gmail.com>
2013-02-04 18:30:50 +0400

    radio fix

--- media/js/m-main.js
+++ media/js/m-main.js
@@ -1,16 +1,16 @@
 var lastScrollTop = 0;
-$(window).bind('scrollstop', function () {
+$(window).bind('scrollstart', function () {
 //    console.log('start');
 //    alert('start');
     var st = $(this).scrollTop();
     if (st > lastScrollTop){
-        $('#buble1').animate({backgroundPositionY:'-=60px'},1530);
-        $('#bg2').animate({backgroundPositionY:'-=10px'},1530);
-        $('body').animate({backgroundPositionY:'-=3px'},1530);
+        $('#buble1').animate({backgroundPositionY:'-=460px'},1530);
+        $('#bg2').animate({backgroundPositionY:'-=200px'},1530);
+        $('body').animate({backgroundPositionY:'-=60px'},1530);
     } else {
-        $('#buble1').animate({backgroundPositionY:'+=60px'},1530);
-        $('#bg2').animate({backgroundPositionY:'+=10px'},1530);
-        $('body').animate({backgroundPositionY:'+=3px'},1530);
+        $('#buble1').animate({backgroundPositionY:'+=460px'},1530);
+        $('#bg2').animate({backgroundPositionY:'+=200px'},1530);
+        $('body').animate({backgroundPositionY:'+=60px'},1530);
     }
     lastScrollTop = st;
 });
